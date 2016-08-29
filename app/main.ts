63673e528c27b79f10fb9ea32a3721081dca2912
Corey Adams <corey.adams2814@gmail.com>
2016-08-29 10:36:03 -0400

    testing vehicles

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,6 @@
   import { bootstrap } from 'angular2/platform/browser';
-  import { CharacterListComponent } from './character-list.component';
+  import { VehiclesComponent } from './vehicles.component';
 
-  bootstrap(CharacterListComponent)
+  bootstrap(VehiclesComponent)
     .then(success => console.log(`Bootstrap success`))
     .catch(error => console.log(error));
-
